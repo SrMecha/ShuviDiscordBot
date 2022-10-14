6949335e723be89fc8a136372b541e70df7e07fc
Sr. Mecha <sr.mecha@bk.ru>
2022-10-14 22:56:37 +0300

    Add simple profile command

--- Shuvi/Enums/UserRaces.cs
+++ Shuvi/Enums/UserRaces.cs
@@ -1,7 +1,22 @@
+using System.Runtime.CompilerServices;
+
 namespace ShuviBot.Enums.UserRaces
 {
     public enum UserRaces
     {
         ExMachina
+    }
+
+    public static class UserRaceEnumExtension
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static string ToRusString(this UserRaces target)
+        {
+            return target switch
+            {
+                UserRaces.ExMachina => "Экс-Машина",
+                _ => "Бог"
+            };
+        }
     }
 }
