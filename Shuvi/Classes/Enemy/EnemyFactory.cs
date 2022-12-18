69a6237f1082aa0a623110a848105dc739901a52
Sr. Mecha <sr.mecha@bk.ru>
2022-12-18 23:39:45 +0300

    Add enemy factory

---
+++ Shuvi/Classes/Enemy/EnemyFactory.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using Shuvi.Interfaces.Entity;
+
+namespace Shuvi.Classes.Enemy
+{
+    public static class EnemyFactory
+    {
+        public static IEnemy GetEnemy(ObjectId id, string picture)
+        {
+            return new EnemyBase(AllEnemiesData.GetData(id), picture);
+        }
+    }
+}
